268a90e71e03881b7cb69ead348e63c4222af013
Jessica Vargas Zanchetta <jessica.zanchetta@ifood.com.br>
2022-03-21 16:45:07 -0300

    commit incial

---
+++ src/main/java/com/durodecodar/desafioilab/DesafioilabApplication.java
@@ -0,0 +1,13 @@
+package com.durodecodar.desafioilab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioilabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioilabApplication.class, args);
+	}
+
+}
