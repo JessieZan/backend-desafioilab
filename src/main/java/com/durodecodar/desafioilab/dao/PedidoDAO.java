b0b595c9aae6907dfa2b2d0ecd5caa2c6c40a52c
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-03-22 11:29:21 -0300

    listas entregadores- pedidos - clientes

--- src/main/java/com/durodecodar/desafioilab/dao/PedidoDAO.java
+++ src/main/java/com/durodecodar/desafioilab/dao/PedidoDAO.java
@@ -2,11 +2,13 @@ package com.durodecodar.desafioilab.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.durodecodar.desafioilab.model.Pedido;
 
 public interface PedidoDAO  extends CrudRepository<Pedido, Integer>{
-
-	public List<Pedido> listarTodosProdutos();
+	
+//	@Query("SELECT new ")
+//	public List<Pedido> listarTodosProdutos();
 }
