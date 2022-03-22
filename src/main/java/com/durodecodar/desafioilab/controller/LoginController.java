61eba4b244bd865dde452738c9e9d79bdd5cc9ea
Jessica Vargas Zanchetta <jessica.zanchetta@ifood.com.br>
2022-03-22 13:30:42 -0300

    Inclusao do DTO entregador login e criação login controller

---
+++ src/main/java/com/durodecodar/desafioilab/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.durodecodar.desafioilab.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.durodecodar.desafioilab.dto.EntregadorLoginDTO;
+
+@RestController
+public class LoginController {
+	
+	//@Autowired
+	//private IUsuarioService service;
+	
+	//@PostMapping("/login")
+	//public ResponseEntity<?> postLogin(@RequestBody EntregadorLoginDTO dadosLogin){
+		//Token t = service.createTokenDeUsuarioLogado(dadosLogin);
+		//if(t != null) {
+			//return ResponseEntity.ok(t);
+		//};
+		
+		//return ResponseEntity.status(400).body("Dados inválidos!");
+		
+	//}
+}
