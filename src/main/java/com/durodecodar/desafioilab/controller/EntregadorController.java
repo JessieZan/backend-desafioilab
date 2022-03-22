6bf8d16a2e879faae13a8e6e1ce78048f906b2b2
Julio Neto <nettojulio@hotmail.com>
2022-03-22 19:27:52 -0300

    CORS adicionado

--- src/main/java/com/durodecodar/desafioilab/controller/EntregadorController.java
+++ src/main/java/com/durodecodar/desafioilab/controller/EntregadorController.java
@@ -3,6 +3,7 @@ package com.durodecodar.desafioilab.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import com.durodecodar.desafioilab.dao.EntregadorDAO;
 import com.durodecodar.desafioilab.model.Entregador;
 
 @RestController
+@CrossOrigin("*")
 public class EntregadorController {
 
 	@Autowired
--- src/main/java/com/durodecodar/desafioilab/controller/LoginController.java
+++ src/main/java/com/durodecodar/desafioilab/controller/LoginController.java
@@ -5,11 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.durodecodar.desafioilab.dto.EntregadorLoginDTO;
 
 @RestController
+@CrossOrigin("*")
 public class LoginController {
 	
 	//@Autowired
